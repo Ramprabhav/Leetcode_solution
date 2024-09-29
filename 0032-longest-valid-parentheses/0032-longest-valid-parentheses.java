115024368ebf0f3d42d1219d23020b8e668fb061
RAMPRABHAV SAHANI <146636218+Ramprabhav@users.noreply.github.com>
2024-09-29 23:54:01 +0530

    Time: 1 ms (99.49%), Space: 42.4 MB (74.84%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        int[] dp = new int[n];
+        int maxLen = 0;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
+    }
+}
